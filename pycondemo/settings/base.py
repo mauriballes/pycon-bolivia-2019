017803076fc93d933309d3898a806324a283fd28
Mauricio Ballesteros <mballes95@gmail.com>
2019-09-13 12:08:26 -0400

    Prepare Deploy

--- pycondemo/settings/base.py
+++ pycondemo/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd Party
+    'djangoup'
 ]
 
 MIDDLEWARE = [
